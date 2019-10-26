cc37f5bc6546e62c7b24c896192c2b763770c504
RD-N1 <erdeni1997@gmail.com>
2019-10-26 09:57:05 +0300

    added: moduel area_engine, u can initialize engine with next parameters: thrust impulse, t_w, chamber pressure, atmosphere pressure, f_a/f_cr

--- charachteristics.py
+++ charachteristics.py
@@ -7,6 +7,5 @@ def specific_impulse(engine, cfuel):
     la = ffd.find_la_p(cfuel.k, engine.p_a, engine.p_c)
     return cfuel.set_beta() * (ffd.impulse(la, cfuel.k) - engine.p_h / engine.p_c) / ffd.mass_flow(la, cfuel.k)
 
-
 if __name__ == "__main__":
     pass
