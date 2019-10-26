fbd9e7259b4071d627a9bcc014cac47720e41db6
RD-N1 <erdeni1997@gmail.com>
2019-10-26 09:43:38 +0300

    removed: kek and tester

--- fuel.py
+++ fuel.py
@@ -15,11 +15,15 @@ class FuelData(object):
         self.R_c = R / mu_cp
 
     def set_beta(self):
-        return sqrt(self.R_c * self.T_c) / a_gdf(self.k)
+        return sqrt(self.R_c * self.T_c) / self.set_a()
+
+    def set_a(self):
+        return sqrt(self.k) * (2 / (self.k + 1)) ** ((self.k + 1) / (2 * (self.k - 1)))
 
     def u_combustion(self, p):
         return self.u_1 * (p / 98066.5) ** self.nu
 
 
 if __name__ == "__main__":
-    pass
+    test_fuel = FuelData(2327, 0.7e-3, 0.7, 1700, 1100, 1.18)
+    print(test_fuel.set_a())
