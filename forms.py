65daa996189c6c4d422952b706c980028f45d603
RD-N1 <erdeni1997@gmail.com>
2019-10-26 21:15:29 +0300

    template for forms

--- forms.py
+++ forms.py
@@ -12,11 +12,38 @@ class OSCCWFE(RESF):
     def __init__(self, fuel, tor):
         super().__init__(tor, fuel)
 
+    def set_din(self):
+        pass
+
+    def set_dout(self):
+        pass
+
+    def set_length(self):
+        pass
+
 
 # channel gap
 class CG(RESF):
     def __init__(self, tor, fuel):
         super().__init__(tor, fuel)
+
+    def set_din(self):
+        pass
+
+    def set_dout(self):
+        pass
+
+    def set_length(self):
+        pass
+
+    def set_gap_lenght(self):
+        pass
+
+    def set_channel_length(self):
+        pass
+
+    def set_gap_width(self):
+        pass
 
 
 # end
@@ -24,11 +51,27 @@ class E(RESF):
     def __init__(self, tor, fuel):
         super().__init__(tor, fuel)
 
+    def set_length(self):
+        pass
+
+    def set_dout(self):
+        pass
+
 
 # multicheckers
 class MC(RESF):
-    def __init__(self, tor, fuel):
+    def __init__(self, tor, fuel, n):
         super().__init__(tor, fuel)
+        self.amount = n
+
+    def set_din(self):
+        pass
+
+    def set_dout(self):
+        pass
+
+    def set_length(self):
+        pass
 
 
 if __name__ == "__main__":
