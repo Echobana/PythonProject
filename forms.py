9899a25119c4b5020a26bcafde6ca9c067ab3c52
RD-N1 <erdeni1997@gmail.com>
2019-10-26 21:10:13 +0300

    module forms

--- forms.py
+++ forms.py
@@ -3,9 +3,35 @@ from fuel import FuelData
 from tor import TORA
 from tor import TOR
 import charachteristics as cta
+from resf import RESF
+import db_handler
 
 
 # one single-channel checkerboard with flat ends
-class OSCCWFE(object):
+class OSCCWFE(RESF):
     def __init__(self, fuel, tor):
-        pass
+        super().__init__(tor, fuel)
+
+
+# channel gap
+class CG(RESF):
+    def __init__(self, tor, fuel):
+        super().__init__(tor, fuel)
+
+
+# end
+class E(RESF):
+    def __init__(self, tor, fuel):
+        super().__init__(tor, fuel)
+
+
+# multicheckers
+class MC(RESF):
+    def __init__(self, tor, fuel):
+        super().__init__(tor, fuel)
+
+
+if __name__ == "__main__":
+    fd_dict = db_handler.db_creator_ground(
+        r'F:\Elizabeth\FuelData\data_ground_1.xlsx')  # indata path is changed, ask RD-N1
+    tor = TORA(1e6, 55, 7e6, 0, 100, 100)
